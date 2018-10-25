2f2d91fda39ce6ed6b9205ac5393d4739a7e7253
Vincent Seyller <vseyller@laposte.net>
2018-10-25 02:41:08 -0400

    Ajout début d'affichage correct xml mois

--- service/statistiques-mois.php
+++ service/statistiques-mois.php
@@ -1,20 +1,41 @@
 <?php 
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
-	include "connexion.php";	
+	include "connexion.php";
+	
+	$SQL_LISTE_CAPTEURS = "SELECT * FROM capteur";
+	$requeteListeCapteurs = $basededonnees->prepare($SQL_LISTE_CAPTEURS);
+	$requeteListeCapteurs->execute();
+	$listeCapteursBD = $requeteListeCapteurs->fetchAll(PDO::FETCH_OBJ);
+	$mois = $_GET['mois'];
+		
+	foreach($listeCapteursBD as $capteur) {
+		$moisCapteur = date("m",strtotime($capteur->date));
+		if($moisCapteur == $mois){
+			$listeDateCapteursParMois[] = $capteur->date;
+			$listeValeurCapteursParMois[] = $capteur->valeur;
+		}
+	}
+	/*
+	for($i = 0; $i<count($listeDateCapteursParMois); $i++){
+		$listeCapteursParJour = array(date("d", strtotime($listeDateCapteursParMois[$i])) => $listeValeurCapteursParMois[$i]);
+		print_r($listeCapteursParJour);
+	}*/
 ?>
 
 <statistiques-mois>
-	<mois></mois>
+	<mois><?=$mois?></mois>
 	<statistiques>
-		<nombre-tests></nombre-tests>
+		<nombre-tests><?=count($listeDateCapteursParMois)?></nombre-tests>
 		<details>
+			<?php /*foreach($listeCapteursParJour as $jour => $valeur) {*/ ?>
 			<jour>
 				<date></date>
 				<moyenne></moyenne>
 				<maximum></maximum>
 				<minimum></minimum>
 			</jour>
+			<?php /*}*/ ?>
 		</details>
 		<synthese>
 			<moyenne-mois></moyenne-mois>
