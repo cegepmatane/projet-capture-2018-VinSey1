e76ea79fecf9edb58dcda528564b61ebb86f6678
Unknown <1643216@cgmatane.qc.ca>
2018-10-31 19:48:10 -0400

    Réception données JSON sur NodeJS

--- service-nodejs/service.js
+++ service-nodejs/service.js
@@ -1,11 +1,20 @@
 var http = require('http');
 
-var serveur = http.createServer(async function(requete, reponse) {	
+var serveur = http.createServer((requete, reponse) => {	
 	if('GET' === requete.method){
 		console.log("Requete GET");
 	}
 	if('POST' === requete.method){
-		console.log("Requete POST");
+        console.log("Requete POST");
+        
+        var uri = '';
+        requete.on('data', message => { uri += message; });
+        requete.on('end', function(){
+            uri = decodeURI(uri);
+            
+            var pollution = JSON.parse(uri).capteur.pollution;
+            console.log(pollution);
+        });
     }
     
     // Termine l'envoi de la demande et si des parties du corps ne sont pas envoyées, 
