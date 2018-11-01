9cb43b2d05061bf48fbdc6c60f826f5e90cd05a3
Vincent Seyller <vseyller@laposte.net>
2018-11-01 02:22:20 -0400

    Affichage statistiques par année fonctionnel

--- service/statistiques-annee.php
+++ service/statistiques-annee.php
@@ -1,25 +1,37 @@
 <?php 
+	include "connexion.php";
+	$annee = $_GET['annee'];
+	$SQL_LISTE_CAPTEURS_ANNEE = "SELECT COUNT(*) as nombreCapteursAnnee, AVG(valeur) as moyenneAnnee, MAX(valeur) as maximumAnnee, MIN(valeur) as minimumAnnee FROM capteur WHERE YEAR(date) =" . $annee;
+	$requeteListeCapteursAnnee = $basededonnees->prepare($SQL_LISTE_CAPTEURS_ANNEE);
+	$requeteListeCapteursAnnee->execute();
+	$listeCapteursAnnee = $requeteListeCapteursAnnee->fetch(PDO::FETCH_OBJ);
+
+	$SQL_LISTE_CAPTEURS_MOIS = "SELECT MONTH(date) as mois, COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE YEAR(date) =" . $annee . " GROUP BY MONTH(date)";
+	$requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
+	$requeteListeCapteursMois->execute();
+	$listeCapteursMois = $requeteListeCapteursMois->fetchAll(PDO::FETCH_OBJ);
+
 	header("Content-type: text/xml");
-	echo '<?xml version="1.0" encoding="UTF-8"?>';
-	include "connexion.php";	
-?>
+	echo '<?xml version="1.0" encoding="UTF-8"?>';?>
 
 <statistiques-annee>
-	<annee></annee>
+	<annee><?=$annee?></annee>
 	<statistiques>
-		<nombre-tests></nombre-tests>
+		<nombre-tests><?=$listeCapteursAnnee->nombreCapteursAnnee?></nombre-tests>
 		<details>
-			<mois>
-				<date></date>
-				<moyenne></moyenne>
-				<maximum></maximum>
-				<minimum></minimum>
-			</mois>
+			<?php foreach($listeCapteursMois as $mois){ ?>
+				<mois>
+					<date><?=$mois->mois?></date>
+					<moyenne><?=$mois->moyenneMois?></moyenne>
+					<maximum><?=$mois->maximumMois?></maximum>
+					<minimum><?=$mois->minimumMois?></minimum>
+				</mois>
+			<?php } ?>
 		</details>
 		<synthese>
-			<moyenne-annee></moyenne-annee>
-			<maximum-annee></maximum-annee>
-			<minimum-annee></minimum-annee>
+			<moyenne-annee><?=$listeCapteursAnnee->moyenneAnnee?></moyenne-annee>
+			<maximum-annee><?=$listeCapteursAnnee->maximumAnnee?></maximum-annee>
+			<minimum-annee><?=$listeCapteursAnnee->minimumAnnee?></minimum-annee>
 			<mois-valeur-max></mois-valeur-max>
 			<mois-valeur-min></mois-valeur-min>
 			<mois-max-tests></mois-max-tests>
