e6a762584b6e9c805b220b4e7ccefe935b9d68a1
Unknown <sedrickidd@hotmail.com>
2018-11-08 18:48:32 -0500

    Implantation du dao dans statistique annee

--- service/statistiques-annee.php
+++ service/statistiques-annee.php
@@ -1,36 +1,22 @@
 <?php 
-	include "connexion.php";
+	include "accesseur/statistiqueDAO.php";
 	$annee = $_GET['annee'];
-	
-	$SQL_LISTE_CAPTEURS_ANNEE = "SELECT COUNT(*) as nombreCapteursAnnee, AVG(valeur) as moyenneAnnee, MAX(valeur) as maximumAnnee, MIN(valeur) as minimumAnnee FROM capteur WHERE YEAR(date) = " . $annee;
-	$requeteListeCapteursAnnee = $basededonnees->prepare($SQL_LISTE_CAPTEURS_ANNEE);
-	$requeteListeCapteursAnnee->execute();
-	$listeCapteursAnnee = $requeteListeCapteursAnnee->fetch(PDO::FETCH_OBJ);
 
-	$SQL_LISTE_CAPTEURS_MOIS = "SELECT MONTH(date) as mois, COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE YEAR(date) = " . $annee . " GROUP BY MONTH(date)";
-	$requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
-	$requeteListeCapteursMois->execute();
-	$listeCapteursMois = $requeteListeCapteursMois->fetchAll(PDO::FETCH_OBJ);
-
-	$SQL_MOIS_VALEUR_MAX = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
-	$requeteMoisValeurMax = $basededonnees->prepare($SQL_MOIS_VALEUR_MAX);
-	$requeteMoisValeurMax->execute();
-	$moisValeurMax = $requeteMoisValeurMax->fetch(PDO::FETCH_OBJ);;
-
-	$SQL_MOIS_VALEUR_MIN = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
-	$requeteMoisValeurMin = $basededonnees->prepare($SQL_MOIS_VALEUR_MIN);
-	$requeteMoisValeurMin->execute();
-	$moisValeurMin = $requeteMoisValeurMin->fetch(PDO::FETCH_OBJ);;
+	$statDao = new StatistiqueDAO;
 	
-	$SQL_MOIS_MAX_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois DESC LIMIT 1";
-    $requeteMoisMaxTests = $basededonnees->prepare($SQL_MOIS_MAX_TESTS);
-    $requeteMoisMaxTests->execute();
-    $moisMaxTests = $requeteMoisMaxTests->fetch(PDO::FETCH_OBJ);;
 	
-	$SQL_MOIS_MIN_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois ASC LIMIT 1";
-    $requeteMoisMinTests = $basededonnees->prepare($SQL_MOIS_MIN_TESTS);
-    $requeteMoisMinTests->execute();
-    $moisMinTests = $requeteMoisMinTests->fetch(PDO::FETCH_OBJ);;
+	$listeCapteursAnnee = $statDao->recevoirListeCapteurAnnee($annee);
+
+	
+	$listeCapteursMois = $statDao->recevoirListeCapteurMoisAnnee($annee);
+
+	$moisValeurMax = $statDao->recevoirMoisValeurMax($annee);
+
+	$moisValeurMin = $statDao->recevoirMoisValeurMin($annee);
+	
+	$moisMaxTests =  $statDao->recevoirMoisMaxTests($annee);
+	
+    $moisMinTests = $statDao->recevoirMoisMinTests($annee);
 	
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
