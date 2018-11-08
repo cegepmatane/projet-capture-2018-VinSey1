97d3d6a69c8dea8a4e90c16f31de2fc5c0f7a6af
Sedrick <sedrickidd@hotmail.com>
2018-11-08 15:39:25 -0500

    Formatage du code des dao

--- service/accesseur/capteurDAO.php
+++ service/accesseur/capteurDAO.php
@@ -4,21 +4,21 @@ class capteurDAO
 {
     function listerCapteurs()
     {
-    global $basededonnees;
-    $SQL_LISTE_CAPTEURS = "SELECT * FROM capteur";
-	$requeteListeCapteurs = $basededonnees->prepare($SQL_LISTE_CAPTEURS);
-	$requeteListeCapteurs->execute();
-    $listeCapteurs = $requeteListeCapteurs->fetchAll(PDO::FETCH_OBJ);
-    return $listeCapteurs;
+        global $basededonnees;
+        $SQL_LISTE_CAPTEURS = "SELECT * FROM capteur";
+	    $requeteListeCapteurs = $basededonnees->prepare($SQL_LISTE_CAPTEURS);
+	    $requeteListeCapteurs->execute();
+        $listeCapteurs = $requeteListeCapteurs->fetchAll(PDO::FETCH_OBJ);
+        return $listeCapteurs;
     }
 
     function recevoirCapteur($id)
     {
-    $SQL_CAPTEUR = "SELECT * FROM capteur WHERE id = " . $id;
-	$requeteCapteur = $basededonnees->prepare($SQL_CAPTEUR);
-	$requeteCapteur->execute();
-    $capteur = $requeteCapteur->fetch(PDO::FETCH_OBJ);
-    return $capteur;
+        $SQL_CAPTEUR = "SELECT * FROM capteur WHERE id = " . $id;
+	    $requeteCapteur = $basededonnees->prepare($SQL_CAPTEUR);
+	    $requeteCapteur->execute();
+        $capteur = $requeteCapteur->fetch(PDO::FETCH_OBJ);
+        return $capteur;
     }
 
 
--- service/accesseur/statistiqueDAO.php
+++ service/accesseur/statistiqueDAO.php
@@ -4,162 +4,162 @@ class statistiqueDAO
 {
     function recevoirListeCapteurAnnee($annee)
     {
-    $SQL_LISTE_CAPTEURS_ANNEE = "SELECT COUNT(*) as nombreCapteursAnnee, AVG(valeur) as moyenneAnnee, MAX(valeur) as maximumAnnee, MIN(valeur) as minimumAnnee FROM capteur WHERE YEAR(date) = " . $annee;
-	$requeteListeCapteursAnnee = $basededonnees->prepare($SQL_LISTE_CAPTEURS_ANNEE);
-	$requeteListeCapteursAnnee->execute();
-    $listeCapteursAnnee = $requeteListeCapteursAnnee->fetch(PDO::FETCH_OBJ);
-    return $listeCapteursAnnee;
+        $SQL_LISTE_CAPTEURS_ANNEE = "SELECT COUNT(*) as nombreCapteursAnnee, AVG(valeur) as moyenneAnnee, MAX(valeur) as maximumAnnee, MIN(valeur) as minimumAnnee FROM capteur WHERE YEAR(date) = " . $annee;
+	    $requeteListeCapteursAnnee = $basededonnees->prepare($SQL_LISTE_CAPTEURS_ANNEE);
+	    $requeteListeCapteursAnnee->execute();
+        $listeCapteursAnnee = $requeteListeCapteursAnnee->fetch(PDO::FETCH_OBJ);
+        return $listeCapteursAnnee;
     }
 
     function recevoirListeCapteurMoisAnnee($annee)
     {
-	$SQL_LISTE_CAPTEURS_MOIS = "SELECT MONTH(date) as mois, COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE YEAR(date) = " . $annee . " GROUP BY MONTH(date)";
-	$requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
-	$requeteListeCapteursMois->execute();
-    $listeCapteursMois = $requeteListeCapteursMois->fetchAll(PDO::FETCH_OBJ);
-    return $listeCapteursMois;
+	    $SQL_LISTE_CAPTEURS_MOIS = "SELECT MONTH(date) as mois, COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE YEAR(date) = " . $annee . " GROUP BY MONTH(date)";
+	    $requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
+	    $requeteListeCapteursMois->execute();
+        $listeCapteursMois = $requeteListeCapteursMois->fetchAll(PDO::FETCH_OBJ);
+        return $listeCapteursMois;
     }
 
     function recevoirMoisValeurMax($annee)
     {
-	$SQL_MOIS_VALEUR_MAX = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
-	$requeteMoisValeurMax = $basededonnees->prepare($SQL_MOIS_VALEUR_MAX);
-	$requeteMoisValeurMax->execute();
-    $moisValeurMax = $requeteMoisValeurMax->fetch(PDO::FETCH_OBJ);
-    return $moisValeurMax;
+	    $SQL_MOIS_VALEUR_MAX = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
+	    $requeteMoisValeurMax = $basededonnees->prepare($SQL_MOIS_VALEUR_MAX);
+	    $requeteMoisValeurMax->execute();
+        $moisValeurMax = $requeteMoisValeurMax->fetch(PDO::FETCH_OBJ);
+        return $moisValeurMax;
     }
 
     function recevoirMoisValeurMin($annee)
     {
-	$SQL_MOIS_VALEUR_MIN = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
-	$requeteMoisValeurMin = $basededonnees->prepare($SQL_MOIS_VALEUR_MIN);
-	$requeteMoisValeurMin->execute();
-    $moisValeurMin = $requeteMoisValeurMin->fetch(PDO::FETCH_OBJ);
-    return $moisValeurMin;
+	    $SQL_MOIS_VALEUR_MIN = "SELECT MONTH(date) as mois, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . ") GROUP BY YEAR(date)";
+	    $requeteMoisValeurMin = $basededonnees->prepare($SQL_MOIS_VALEUR_MIN);
+	    $requeteMoisValeurMin->execute();
+        $moisValeurMin = $requeteMoisValeurMin->fetch(PDO::FETCH_OBJ);
+        return $moisValeurMin;
     }
 
     function recevoirMoisMaxTests($annee)
     {
-	$SQL_MOIS_MAX_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois DESC LIMIT 1";
-    $requeteMoisMaxTests = $basededonnees->prepare($SQL_MOIS_MAX_TESTS);
-    $requeteMoisMaxTests->execute();
-    $moisMaxTests = $requeteMoisMaxTests->fetch(PDO::FETCH_OBJ);
-    return $moisMaxTests;
+	    $SQL_MOIS_MAX_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois DESC LIMIT 1";
+        $requeteMoisMaxTests = $basededonnees->prepare($SQL_MOIS_MAX_TESTS);
+        $requeteMoisMaxTests->execute();
+        $moisMaxTests = $requeteMoisMaxTests->fetch(PDO::FETCH_OBJ);
+        return $moisMaxTests;
     }
 
     function recevoirMoisMinTests($annee)
     {
-	$SQL_MOIS_MIN_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois ASC LIMIT 1";
-    $requeteMoisMinTests = $basededonnees->prepare($SQL_MOIS_MIN_TESTS);
-    $requeteMoisMinTests->execute();
-    $moisMinTests = $requeteMoisMinTests->fetch(PDO::FETCH_OBJ);
-    return $moisMinTests;
+	    $SQL_MOIS_MIN_TESTS = "SELECT MONTH(date) as mois FROM (SELECT COUNT(valeur) as nombreCapteursMois, date FROM capteur GROUP BY MONTH(date)) as nombreTests WHERE YEAR(date) =" . $annee . " ORDER BY nombreCapteursMois ASC LIMIT 1";
+        $requeteMoisMinTests = $basededonnees->prepare($SQL_MOIS_MIN_TESTS);
+        $requeteMoisMinTests->execute();
+        $moisMinTests = $requeteMoisMinTests->fetch(PDO::FETCH_OBJ);
+        return $moisMinTests;
     }
 
     function recevoirListeCapteurMois($mois, $annee)
     {
-    $SQL_LISTE_CAPTEURS_MOIS = "SELECT COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE MONTH(date) = " . $mois . " AND YEAR(date) = " . $annee;
-	$requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
-	$requeteListeCapteursMois->execute();
-    $listeCapteursMois = $requeteListeCapteursMois->fetch(PDO::FETCH_OBJ);
-    return $listeCapteursMois;
+        $SQL_LISTE_CAPTEURS_MOIS = "SELECT COUNT(*) as nombreCapteursMois, AVG(valeur) as moyenneMois, MAX(valeur) as maximumMois, MIN(valeur) as minimumMois FROM capteur WHERE MONTH(date) = " . $mois . " AND YEAR(date) = " . $annee;
+	    $requeteListeCapteursMois = $basededonnees->prepare($SQL_LISTE_CAPTEURS_MOIS);
+	    $requeteListeCapteursMois->execute();
+        $listeCapteursMois = $requeteListeCapteursMois->fetch(PDO::FETCH_OBJ);
+        return $listeCapteursMois;
     }
 
     function recevoirListeCapteurJourMois($mois, $annee)
     {
-	$SQL_LISTE_CAPTEURS_JOUR = "SELECT DAY(date) as jour, COUNT(*) as nombreCapteursJour, AVG(valeur) as moyenneJour, MAX(valeur) as maximumJour, MIN(valeur) as minimumJour FROM capteur WHERE MONTH(date) = " . $mois . " AND YEAR(date) = " . $annee . " GROUP BY DAY(date)";
-	$requeteListeCapteursJour = $basededonnees->prepare($SQL_LISTE_CAPTEURS_JOUR);
-	$requeteListeCapteursJour->execute();
-	$listeCapteursJour = $requeteListeCapteursJour->fetchAll(PDO::FETCH_OBJ);
-    return $listeCapteursJour;
+	    $SQL_LISTE_CAPTEURS_JOUR = "SELECT DAY(date) as jour, COUNT(*) as nombreCapteursJour, AVG(valeur) as moyenneJour, MAX(valeur) as maximumJour, MIN(valeur) as minimumJour FROM capteur WHERE MONTH(date) = " . $mois . " AND YEAR(date) = " . $annee . " GROUP BY DAY(date)";
+	    $requeteListeCapteursJour = $basededonnees->prepare($SQL_LISTE_CAPTEURS_JOUR);
+	    $requeteListeCapteursJour->execute();
+	    $listeCapteursJour = $requeteListeCapteursJour->fetchAll(PDO::FETCH_OBJ);
+        return $listeCapteursJour;
     }
     function recevoirJoursValeurMax($mois, $annee)
     {
-	$SQL_JOUR_VALEUR_MAX = "SELECT DAY(date) as jour, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . ") GROUP BY MONTH(date)";
-	$requeteJourValeurMax = $basededonnees->prepare($SQL_JOUR_VALEUR_MAX);
-	$requeteJourValeurMax->execute();
-	$jourValeurMax = $requeteJourValeurMax->fetch(PDO::FETCH_OBJ);
-    return $jourValeurMax;
+	    $SQL_JOUR_VALEUR_MAX = "SELECT DAY(date) as jour, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . ") GROUP BY MONTH(date)";
+	    $requeteJourValeurMax = $basededonnees->prepare($SQL_JOUR_VALEUR_MAX);
+	    $requeteJourValeurMax->execute();
+	    $jourValeurMax = $requeteJourValeurMax->fetch(PDO::FETCH_OBJ);
+        return $jourValeurMax;
     }
     
     function recevoirJourValeurMin($mois, $annee)
     {
-	$SQL_JOUR_VALEUR_MIN = "SELECT DAY(date) as jour, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . ") GROUP BY MONTH(date)";
-	$requeteJourValeurMin = $basededonnees->prepare($SQL_JOUR_VALEUR_MIN);
-	$requeteJourValeurMin->execute();
-	$jourValeurMin = $requeteJourValeurMin->fetch(PDO::FETCH_OBJ);
-    return $jourValeurMin;
+	    $SQL_JOUR_VALEUR_MIN = "SELECT DAY(date) as jour, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . ") GROUP BY MONTH(date)";
+	    $requeteJourValeurMin = $basededonnees->prepare($SQL_JOUR_VALEUR_MIN);
+	    $requeteJourValeurMin->execute();
+	    $jourValeurMin = $requeteJourValeurMin->fetch(PDO::FETCH_OBJ);
+        return $jourValeurMin;
     }
 
     function recevoirJourMaxTests($mois, $annee)
     {
-	$SQL_JOUR_MAX_TESTS = "SELECT DAY(date) as jour FROM (SELECT COUNT(valeur) as nombreCapteursJour, date FROM capteur GROUP BY DAY(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " ORDER BY nombreCapteursJour DESC LIMIT 1";
-    $requeteJourMaxTests = $basededonnees->prepare($SQL_JOUR_MAX_TESTS);
-    $requeteJourMaxTests->execute();
-    $jourMaxTests = $requeteJourMaxTests->fetch(PDO::FETCH_OBJ);
-    return $jourMaxTests;
+	    $SQL_JOUR_MAX_TESTS = "SELECT DAY(date) as jour FROM (SELECT COUNT(valeur) as nombreCapteursJour, date FROM capteur GROUP BY DAY(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " ORDER BY nombreCapteursJour DESC LIMIT 1";
+        $requeteJourMaxTests = $basededonnees->prepare($SQL_JOUR_MAX_TESTS);
+        $requeteJourMaxTests->execute();
+        $jourMaxTests = $requeteJourMaxTests->fetch(PDO::FETCH_OBJ);
+        return $jourMaxTests;
     }
     
     function recevoirJourMinTests($mois, $annee)
     {
-	$SQL_JOUR_MIN_TESTS = "SELECT DAY(date) as jour FROM (SELECT COUNT(valeur) as nombreCapteursJour, date FROM capteur GROUP BY DAY(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " ORDER BY nombreCapteursJour ASC LIMIT 1";
-    $requeteJourMinTests = $basededonnees->prepare($SQL_JOUR_MIN_TESTS);
-    $requeteJourMinTests->execute();
-    $jourMinTests = $requeteJourMinTests->fetch(PDO::FETCH_OBJ);;
-    return $jourMinTests;
+	    $SQL_JOUR_MIN_TESTS = "SELECT DAY(date) as jour FROM (SELECT COUNT(valeur) as nombreCapteursJour, date FROM capteur GROUP BY DAY(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " ORDER BY nombreCapteursJour ASC LIMIT 1";
+        $requeteJourMinTests = $basededonnees->prepare($SQL_JOUR_MIN_TESTS);
+        $requeteJourMinTests->execute();
+        $jourMinTests = $requeteJourMinTests->fetch(PDO::FETCH_OBJ);;
+        return $jourMinTests;
     }
     
     function recevoirListeCapteurJour($mois, $annee, $jour)
     {
-    $SQL_LISTE_CAPTEURS_JOUR = "SELECT COUNT(*) as nombreCapteursJour, AVG(valeur) as moyenneJour, MAX(valeur) as maximumJour, MIN(valeur) as minimumJour FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour;
-	$requeteListeCapteursJour = $basededonnees->prepare($SQL_LISTE_CAPTEURS_JOUR);
-	$requeteListeCapteursJour->execute();
-	$listeCapteursJour = $requeteListeCapteursJour->fetch(PDO::FETCH_OBJ);
-    return $listeCapteursJour;
+        $SQL_LISTE_CAPTEURS_JOUR = "SELECT COUNT(*) as nombreCapteursJour, AVG(valeur) as moyenneJour, MAX(valeur) as maximumJour, MIN(valeur) as minimumJour FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour;
+	    $requeteListeCapteursJour = $basededonnees->prepare($SQL_LISTE_CAPTEURS_JOUR);
+	    $requeteListeCapteursJour->execute();
+	    $listeCapteursJour = $requeteListeCapteursJour->fetch(PDO::FETCH_OBJ);
+        return $listeCapteursJour;
     }
 
     function recevoirListeCapteurHeure($mois, $annee, $jour)
     {
-	$SQL_LISTE_CAPTEURS_HEURE = "SELECT HOUR(date) as heure, COUNT(*) as nombreCapteursHeure, AVG(valeur) as moyenneHeure, MAX(valeur) as maximumHeure, MIN(valeur) as minimumHeure FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " GROUP BY HOUR(date)";
-	$requeteListeCapteursHeure = $basededonnees->prepare($SQL_LISTE_CAPTEURS_HEURE);
-	$requeteListeCapteursHeure->execute();
-	$listeCapteursHeure = $requeteListeCapteursHeure->fetchAll(PDO::FETCH_OBJ);
-    return $listeCapteursHeure;
+	    $SQL_LISTE_CAPTEURS_HEURE = "SELECT HOUR(date) as heure, COUNT(*) as nombreCapteursHeure, AVG(valeur) as moyenneHeure, MAX(valeur) as maximumHeure, MIN(valeur) as minimumHeure FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " GROUP BY HOUR(date)";
+	    $requeteListeCapteursHeure = $basededonnees->prepare($SQL_LISTE_CAPTEURS_HEURE);
+	    $requeteListeCapteursHeure->execute();
+	    $listeCapteursHeure = $requeteListeCapteursHeure->fetchAll(PDO::FETCH_OBJ);
+        return $listeCapteursHeure;
     }
 
     function recevoirHeureValeurMax($mois, $annee, $jour)
     {
-	$SQL_HEURE_VALEUR_MAX = "SELECT HOUR(date) as heure, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . ") GROUP BY DAY(date)";
-	$requeteHeureValeurMax = $basededonnees->prepare($SQL_HEURE_VALEUR_MAX);
-	$requeteHeureValeurMax->execute();
-	$heureValeurMax = $requeteHeureValeurMax->fetch(PDO::FETCH_OBJ);
-    return $heureValeurMax;
+	    $SQL_HEURE_VALEUR_MAX = "SELECT HOUR(date) as heure, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " AND valeur IN (SELECT MAX(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . ") GROUP BY DAY(date)";
+	    $requeteHeureValeurMax = $basededonnees->prepare($SQL_HEURE_VALEUR_MAX);
+	    $requeteHeureValeurMax->execute();
+	    $heureValeurMax = $requeteHeureValeurMax->fetch(PDO::FETCH_OBJ);
+        return $heureValeurMax;
     }
 
     function recevoirHeureValeurMin($mois, $annee, $jour)
     {
-	$SQL_HEURE_VALEUR_MIN = "SELECT HOUR(date) as heure, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . ") GROUP BY DAY(date)";
-	$requeteHeureValeurMin = $basededonnees->prepare($SQL_HEURE_VALEUR_MIN);
-	$requeteHeureValeurMin->execute();
-	$heureValeurMin = $requeteHeureValeurMin->fetch(PDO::FETCH_OBJ);
-    return $heureValeurMin;
+	    $SQL_HEURE_VALEUR_MIN = "SELECT HOUR(date) as heure, valeur FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " AND valeur IN (SELECT MIN(valeur) FROM capteur WHERE YEAR(date) = " . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . ") GROUP BY DAY(date)";
+	    $requeteHeureValeurMin = $basededonnees->prepare($SQL_HEURE_VALEUR_MIN);
+	    $requeteHeureValeurMin->execute();
+	    $heureValeurMin = $requeteHeureValeurMin->fetch(PDO::FETCH_OBJ);
+        return $heureValeurMin;
     }
 
     function recevoirHeureMaxTests($mois, $annee, $jour)
     {
-	$SQL_HEURE_MAX_TESTS = "SELECT HOUR(date) as heure FROM (SELECT COUNT(valeur) as nombreCapteursHeure, date FROM capteur GROUP BY HOUR(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " ORDER BY nombreCapteursHeure DESC LIMIT 1";
-    $requeteHeureMaxTests = $basededonnees->prepare($SQL_HEURE_MAX_TESTS);
-    $requeteHeureMaxTests->execute();
-    $heureMaxTests = $requeteHeureMaxTests->fetch(PDO::FETCH_OBJ);
+	    $SQL_HEURE_MAX_TESTS = "SELECT HOUR(date) as heure FROM (SELECT COUNT(valeur) as nombreCapteursHeure, date FROM capteur GROUP BY HOUR(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " ORDER BY nombreCapteursHeure DESC LIMIT 1";
+        $requeteHeureMaxTests = $basededonnees->prepare($SQL_HEURE_MAX_TESTS);
+        $requeteHeureMaxTests->execute();
+        $heureMaxTests = $requeteHeureMaxTests->fetch(PDO::FETCH_OBJ);
     }
 
     function recevoirHeureMinTests($mois, $annee, $jour)
     {
-	$SQL_HEURE_MIN_TESTS = "SELECT HOUR(date) as heure FROM (SELECT COUNT(valeur) as nombreCapteursHeure, date FROM capteur GROUP BY HOUR(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " ORDER BY nombreCapteursHeure ASC LIMIT 1";
-    $requeteHeureMinTests = $basededonnees->prepare($SQL_HEURE_MIN_TESTS);
-    $requeteHeureMinTests->execute();
-    $heureMinTests = $requeteHeureMinTests->fetch(PDO::FETCH_OBJ);
-    return $heureMinTests;
+	    $SQL_HEURE_MIN_TESTS = "SELECT HOUR(date) as heure FROM (SELECT COUNT(valeur) as nombreCapteursHeure, date FROM capteur GROUP BY HOUR(date)) as nombreTests WHERE YEAR(date) =" . $annee . " AND MONTH(date) = " . $mois . " AND DAY(date) = " . $jour . " ORDER BY nombreCapteursHeure ASC LIMIT 1";
+        $requeteHeureMinTests = $basededonnees->prepare($SQL_HEURE_MIN_TESTS);
+        $requeteHeureMinTests->execute();
+        $heureMinTests = $requeteHeureMinTests->fetch(PDO::FETCH_OBJ);
+        return $heureMinTests;
     }
 }
 
