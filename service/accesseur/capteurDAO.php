dc88628621feafb597b26ffa78a6db168b459e83
Sedrick <sedrickidd@hotmail.com>
2018-11-08 15:35:15 -0500

    Dao des capteurs est prêt a être implantée

---
+++ service/accesseur/capteurDAO.php
@@ -0,0 +1,33 @@
+<?php
+include "connexion.php";
+class capteurDAO
+{
+    function listerCapteurs()
+    {
+    global $basededonnees;
+    $SQL_LISTE_CAPTEURS = "SELECT * FROM capteur";
+	$requeteListeCapteurs = $basededonnees->prepare($SQL_LISTE_CAPTEURS);
+	$requeteListeCapteurs->execute();
+    $listeCapteurs = $requeteListeCapteurs->fetchAll(PDO::FETCH_OBJ);
+    return $listeCapteurs;
+    }
+
+    function recevoirCapteur($id)
+    {
+    $SQL_CAPTEUR = "SELECT * FROM capteur WHERE id = " . $id;
+	$requeteCapteur = $basededonnees->prepare($SQL_CAPTEUR);
+	$requeteCapteur->execute();
+    $capteur = $requeteCapteur->fetch(PDO::FETCH_OBJ);
+    return $capteur;
+    }
+
+
+
+
+
+}
+
+
+
+
+?>
