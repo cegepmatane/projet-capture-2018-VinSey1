e2940d8bae096371a7db3a880c4f145d659086b1
Unknown <1643216@cgmatane.qc.ca>
2018-11-11 20:43:16 -0500

    Nettoyage du code et reponse au client lors de requete

--- service-nodejs/Service.js
+++ service-nodejs/Service.js
@@ -1,24 +1,27 @@
 var http = require('http');
 var capteurDAO = require('./accesseur/CapteurDAO');
 
-var serveur = http.createServer((requete, reponse) => {	
+var serveur = http.createServer((requete, reponse) => {
+	reponse.writeHead(200, { 'Content-Type': 'text/plain' });
+	
 	if('GET' === requete.method){
 		console.log("Requete GET");
+		reponse.end('Requete GET recu par le serveur NodeJS');
 	}
 	if('POST' === requete.method){
-        console.log("Requete POST");
+	        console.log("Requete POST");
+		reponse.end("Requete POST recu par le serveur NodeJS");
         
-        var uri = '';
-        requete.on('data', message => { uri += message; });
-        requete.on('end', function(){
-            uri = decodeURI(uri);
+        	var uri = '';
+        	requete.on('data', message => { uri += message; });
+	        requete.on('end', function(){
+			uri = decodeURI(uri);
             
-            var capteur = JSON.parse(uri).capteur;
-            capteurDAO.ajouterCapteur(capteur);
+			var capteur = JSON.parse(uri).capteur;
+			capteurDAO.ajouterCapteur(capteur);
         });
     }
-    
     // Termine l'envoi de la demande et si des parties du corps ne sont pas envoyées, 
     // cela les jettera dans le flux.
-	reponse.end('');
+    reponse.end('');
 }).listen(8080);
