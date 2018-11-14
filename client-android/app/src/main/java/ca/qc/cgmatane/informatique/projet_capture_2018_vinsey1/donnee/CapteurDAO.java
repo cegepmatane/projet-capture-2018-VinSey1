28b6963afa152b78ae18210a7bffcf4970f27314
Vincent Seyller <vseyller@laposte.net>
2018-11-13 21:51:40 -0500

    Ajout récupération XML CapteurDAO

--- client-android/app/src/main/java/ca/qc/cgmatane/informatique/projet_capture_2018_vinsey1/donnee/CapteurDAO.java
+++ client-android/app/src/main/java/ca/qc/cgmatane/informatique/projet_capture_2018_vinsey1/donnee/CapteurDAO.java
@@ -1,9 +1,30 @@
 package ca.qc.cgmatane.informatique.projet_capture_2018_vinsey1.donnee;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import ca.qc.cgmatane.informatique.projet_capture_2018_vinsey1.modele.Heure;
+
 public class CapteurDAO {
 
     private static CapteurDAO instance = null;
     private ServiceDAO accesseurService;
+    private String xml, nombreTests, moyenneJour, maximumJour, minimumJour, heureValeurMax, heureValeurMin, heureMaximumTests, heureMinimumTests;
+    private List<Heure> listeHeures;
 
     public static CapteurDAO getInstance(){
         if(null == instance){
@@ -12,7 +33,93 @@ public class CapteurDAO {
         return instance;
     }
 
-    public CapteurDAO{
+    public CapteurDAO() {
         accesseurService = new ServiceDAO();
+        String date = new SimpleDateFormat("dd/MM/yyyy").format(System.currentTimeMillis());
+        String[] dateCoupee = date.split("/");
+        try {
+            xml = accesseurService.execute("http://158.69.192.249/pollution/moyenne/annee/"+dateCoupee[2]+"/mois/"+dateCoupee[1]+"/jour/"+dateCoupee[0], "</statistiques-jour>").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            initialiserStatistiques();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initialiserStatistiques() throws ParserConfigurationException, IOException, SAXException {
+        listeHeures = new ArrayList<>();
+        DocumentBuilder parseur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = parseur.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        NodeList listeNoeudHeures = document.getElementsByTagName("heure");
+
+        for(int iterateur = 0; iterateur < listeNoeudHeures.getLength(); iterateur++){
+            Element noeudHeure = (Element) listeNoeudHeures.item(iterateur);
+
+            String horaire = noeudHeure.getElementsByTagName("horaire").item(0).getTextContent();
+            String moyenne = noeudHeure.getElementsByTagName("moyenne").item(0).getTextContent();
+            String maximum = noeudHeure.getElementsByTagName("maximum").item(0).getTextContent();
+            String minimum = noeudHeure.getElementsByTagName("minimum").item(0).getTextContent();
+
+            Heure heure = new Heure(horaire, moyenne, maximum, minimum);
+
+            listeHeures.add(heure);
+        }
+
+        moyenneJour = document.getElementsByTagName("moyenne-jour").item(0).getTextContent();
+        maximumJour = document.getElementsByTagName("maximum-jour").item(0).getTextContent();
+        minimumJour = document.getElementsByTagName("minimum-jour").item(0).getTextContent();
+        heureValeurMax = document.getElementsByTagName("heure-valeur-max").item(0).getTextContent();
+        heureValeurMin = document.getElementsByTagName("heure-valeur-min").item(0).getTextContent();
+        heureMaximumTests = document.getElementsByTagName("heure-max-tests").item(0).getTextContent();
+        heureMinimumTests = document.getElementsByTagName("heure-min-tests").item(0).getTextContent();
+    }
+
+    public List<HashMap<String,String>> recupererListeHeuresPourAdapteur() {
+        List<HashMap<String, String>> listeHeuresPourAdaptateur = new ArrayList<HashMap<String, String>>();
+        for(Heure heure : listeHeures){
+            listeHeuresPourAdaptateur.add(heure.obtenirHeurePourAdapteur());
+        }
+        return listeHeuresPourAdaptateur;
+    }
+
+    public String getNombreTests() {
+        return nombreTests;
+    }
+
+    public String getMoyenneJour() {
+        return moyenneJour;
+    }
+
+    public String getMaximumJour() {
+        return maximumJour;
+    }
+
+    public String getMinimumJour() {
+        return minimumJour;
+    }
+
+    public String getHeureValeurMax() {
+        return heureValeurMax;
+    }
+
+    public String getHeureValeurMin() {
+        return heureValeurMin;
+    }
+
+    public String getHeureMaximumTests() {
+        return heureMaximumTests;
+    }
+
+    public String getHeureMinimumTests() {
+        return heureMinimumTests;
     }
 }
