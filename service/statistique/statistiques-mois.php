a7427dfd2936b2e7afdd5fd3cedabdba9a7a9d8b
Vincent Seyller <vseyller@laposte.net>
2018-11-18 21:13:25 -0500

    Ajout PHP_FILTER statistiques-mois

--- service/statistique/statistiques-mois.php
+++ service/statistique/statistiques-mois.php
@@ -20,49 +20,53 @@
 	{
 		$maxTestJour = $listeCapteursJour[0]->nombreCapteursJour;
 		$minTestJour = $listeCapteursJour[0]->nombreCapteursJour;
-
 	}
 	
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+	$arguments = array($annee, $mois);
+
+	if(filter_var_array($arguments, FILTER_VALIDATE_INT)){
 ?>
-<statistiques-mois>
-	<mois><?=$mois . "/" . $annee?></mois>
-	<statistiques>
-		<nombre-tests><?=$listeCapteursMois->nombreCapteursMois?></nombre-tests>
-		<details>
-			<?php foreach($listeCapteursJour as $jour){
 
-				if(($jour->nombreCapteursJour)>$maxTestJour)
-				{
-					$maxTestJour = $jour->nombreCapteursJour;
-					
-				}
-				if(($jour->nombreCapteursJour)<$minTestJour)
-				{
-					$minTestJour = $jour->nombreCapteursJour;
-					
-				}
+	<statistiques-mois>
+		<mois><?=$mois . "/" . $annee?></mois>
+		<statistiques>
+			<nombre-tests><?=$listeCapteursMois->nombreCapteursMois?></nombre-tests>
+			<details>
+				<?php foreach($listeCapteursJour as $jour){
 
-				?>
-				<jour>
-					<date><?=$jour->jour?></date>
-					<moyenne><?=$jour->moyenneJour?></moyenne>
-					<maximum><?=$jour->maximumJour?></maximum>
-					<minimum><?=$jour->minimumJour?></minimum>
-				</jour>
-			<?php } ?>
-		</details>
-		<synthese>
-			<moyenne-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->moyenneMois:"";?></moyenne-mois>
-			<maximum-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->maximumMois:"";?></maximum-mois>
-			<minimum-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->minimumMois:"";?></minimum-mois>
-			<jour-valeur-max><?=($jourValeurMax !=null) ? $jourValeurMax->jour:"";?></jour-valeur-max>
-			<jour-valeur-min><?=($jourValeurMin !=null) ? $jourValeurMin->jour:"";?></jour-valeur-min>
-			<jour-max-test><?=($jourMaxTests !=null) ? $jourMaxTests->jour:"";?></jour-max-test>
-			<jour-min-test><?=($jourMinTests !=null) ? $jourMinTests->jour:"";?></jour-min-test>
+					if(($jour->nombreCapteursJour)>$maxTestJour)
+					{
+						$maxTestJour = $jour->nombreCapteursJour;
+					}
+					if(($jour->nombreCapteursJour)<$minTestJour)
+					{
+						$minTestJour = $jour->nombreCapteursJour;
+					}
 
-			
-		</synthese>
-	</statistiques>
-</statistiques-mois>
+					?>
+					<jour>
+						<date><?=$jour->jour?></date>
+						<moyenne><?=$jour->moyenneJour?></moyenne>
+						<maximum><?=$jour->maximumJour?></maximum>
+						<minimum><?=$jour->minimumJour?></minimum>
+					</jour>
+				<?php } ?>
+			</details>
+			<synthese>
+				<moyenne-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->moyenneMois:"";?></moyenne-mois>
+				<maximum-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->maximumMois:"";?></maximum-mois>
+				<minimum-mois><?=($listeCapteursMois !=null) ? $listeCapteursMois->minimumMois:"";?></minimum-mois>
+				<jour-valeur-max><?=($jourValeurMax !=null) ? $jourValeurMax->jour:"";?></jour-valeur-max>
+				<jour-valeur-min><?=($jourValeurMin !=null) ? $jourValeurMin->jour:"";?></jour-valeur-min>
+				<jour-max-test><?=($jourMaxTests !=null) ? $jourMaxTests->jour:"";?></jour-max-test>
+				<jour-min-test><?=($jourMinTests !=null) ? $jourMinTests->jour:"";?></jour-min-test>
+			</synthese>
+		</statistiques>
+	</statistiques-mois>
+
+<?php } else {
+	echo("Les arguments ne sont pas valides");
+} ?>
