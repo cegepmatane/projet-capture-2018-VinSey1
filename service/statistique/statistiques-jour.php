321d6860ef9819fe7b36262ad5d40b28f342e5bf
Vincent Seyller <vseyller@laposte.net>
2018-11-18 21:13:21 -0500

    Ajout PHP_FILTER statistiques-jour

--- service/statistique/statistiques-jour.php
+++ service/statistique/statistiques-jour.php
@@ -25,41 +25,49 @@
 
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+	$arguments = array($annee, $mois, $jour);
+
+	if(filter_var_array($arguments, FILTER_VALIDATE_INT)){
 ?>
 
-<statistiques-jour>
-	<jour><?=$jour . "/" . $mois . "/" . $annee?></jour>
-	<statistiques>
-		<nombre-tests><?=$listeCapteursJour->nombreCapteursJour?></nombre-tests>
-		<details>
-			<?php foreach($listeCapteursHeure as $heure){ 
+	<statistiques-jour>
+		<jour><?=$jour . "/" . $mois . "/" . $annee?></jour>
+		<statistiques>
+			<nombre-tests><?=$listeCapteursJour->nombreCapteursJour?></nombre-tests>
+			<details>
+				<?php foreach($listeCapteursHeure as $heure){ 
 
-				if(($heure->nombreCapteursHeure)>$maxTestHeure)
-				{
-					$maxTestHeure = $heure->nombreCapteursHeure;					
-				}
-				if(($heure->nombreCapteursHeure)<$minTestHeure)
-				{
-					$minTestHeure = $heure->nombreCapteursHeure;					
-				}			
+					if(($heure->nombreCapteursHeure)>$maxTestHeure)
+					{
+						$maxTestHeure = $heure->nombreCapteursHeure;					
+					}
+					if(($heure->nombreCapteursHeure)<$minTestHeure)
+					{
+						$minTestHeure = $heure->nombreCapteursHeure;					
+					}			
 				
-				?>
-				<heure>
-					<horaire><?=$heure->heure?></horaire>
-					<moyenne><?=$heure->moyenneHeure?></moyenne>
-					<maximum><?=$heure->maximumHeure?></maximum>
-					<minimum><?=$heure->minimumHeure?></minimum>
-				</heure>
-			<?php } ?>
-		</details>
-		<synthese>		
-			<moyenne-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->moyenneJour:"";?></moyenne-jour>
-			<maximum-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->maximumJour:"";?></maximum-jour>
-			<minimum-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->minimumJour:"";?></minimum-jour>
-			<heure-valeur-max><?=($heureValeurMax !=null) ? $heureValeurMax->heure:"";?></heure-valeur-max>
-			<heure-valeur-min><?=($heureValeurMin !=null) ? $heureValeurMin->heure:"";?></heure-valeur-min>
-			<heure-max-tests><?=($heureMaxTests !=null) ? $heureMaxTests->heure:"";?></heure-max-tests>
-			<heure-min-tests><?=($heureMinTests !=null) ? $heureMinTests->heure:"";?></heure-min-tests>
-		</synthese>
-	</statistiques>
-</statistiques-jour>
+					?>
+					<heure>
+						<horaire><?=$heure->heure?></horaire>
+						<moyenne><?=$heure->moyenneHeure?></moyenne>
+						<maximum><?=$heure->maximumHeure?></maximum>
+						<minimum><?=$heure->minimumHeure?></minimum>
+					</heure>
+				<?php } ?>
+			</details>
+			<synthese>		
+				<moyenne-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->moyenneJour:"";?></moyenne-jour>
+				<maximum-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->maximumJour:"";?></maximum-jour>
+				<minimum-jour><?=($listeCapteursJour !=null) ? $listeCapteursJour->minimumJour:"";?></minimum-jour>
+				<heure-valeur-max><?=($heureValeurMax !=null) ? $heureValeurMax->heure:"";?></heure-valeur-max>
+				<heure-valeur-min><?=($heureValeurMin !=null) ? $heureValeurMin->heure:"";?></heure-valeur-min>
+				<heure-max-tests><?=($heureMaxTests !=null) ? $heureMaxTests->heure:"";?></heure-max-tests>
+				<heure-min-tests><?=($heureMinTests !=null) ? $heureMinTests->heure:"";?></heure-min-tests>
+			</synthese>
+		</statistiques>
+	</statistiques-jour>
+
+<?php } else {
+	echo("Les arguments ne sont pas valides");
+} ?>
