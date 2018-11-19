8ce360d6d3ed2f782bf7101fc6ddb89ce645ffe6
Vincent Seyller <vseyller@laposte.net>
2018-11-18 21:13:12 -0500

    Ajout PHP_FILTER statistiques-annee

--- service/statistique/statistiques-annee.php
+++ service/statistique/statistiques-annee.php
@@ -4,10 +4,8 @@
 
 	$statDao = new StatistiqueDAO;
 	
-	
 	$listeCapteursAnnee = $statDao->recevoirListeCapteurAnnee($annee);
 
-	
 	$listeCapteursMois = $statDao->recevoirListeCapteurMoisAnnee($annee);
 
 	$moisValeurMax = $statDao->recevoirMoisValeurMax($annee);
@@ -20,30 +18,36 @@
 	
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+	if(filter_var($annee, FILTER_VALIDATE_INT)){
 ?>
 
-<statistiques-annee>
-	<annee><?=$annee?></annee>
-	<statistiques>
-		<nombre-tests><?=$listeCapteursAnnee->nombreCapteursAnnee?></nombre-tests>
-		<details>
-			<?php foreach($listeCapteursMois as $mois){ ?>
-				<mois>
-					<date><?=$mois->mois?></date>
-					<moyenne><?=$mois->moyenneMois?></moyenne>
-					<maximum><?=$mois->maximumMois?></maximum>
-					<minimum><?=$mois->minimumMois?></minimum>
-				</mois>
-			<?php } ?>
-		</details>
-		<synthese>
-			<moyenne-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->moyenneAnnee :"";?></moyenne-annee>
-			<maximum-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->maximumAnnee:"";?></maximum-annee>
-			<minimum-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->minimumAnnee:""?></minimum-annee>
-			<mois-valeur-max><?=($moisValeurMax!=null) ? $moisValeurMax->mois:"";?></mois-valeur-max>
-			<mois-valeur-min><?=($moisValeurMin!=null) ? $moisValeurMin->mois:"";?></mois-valeur-min>
-			<mois-max-tests><?=($moisMaxTests!=null) ? $moisMaxTests->mois:"";?></mois-max-tests>
-			<mois-min-tests><?=($moisMinTests!=null) ? $moisMinTests->mois:"";?></mois-min-tests>
-		</synthese>
-	</statistiques>
-</statistiques-annee>
+	<statistiques-annee>
+		<annee><?=$annee?></annee>
+		<statistiques>
+			<nombre-tests><?=$listeCapteursAnnee->nombreCapteursAnnee?></nombre-tests>
+			<details>
+				<?php foreach($listeCapteursMois as $mois){ ?>
+					<mois>
+						<date><?=$mois->mois?></date>
+						<moyenne><?=$mois->moyenneMois?></moyenne>
+						<maximum><?=$mois->maximumMois?></maximum>
+						<minimum><?=$mois->minimumMois?></minimum>
+					</mois>
+				<?php } ?>
+			</details>
+			<synthese>
+				<moyenne-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->moyenneAnnee :"";?></moyenne-annee>
+				<maximum-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->maximumAnnee:"";?></maximum-annee>
+				<minimum-annee><?=($listeCapteursAnnee!=null) ? $listeCapteursAnnee->minimumAnnee:""?></minimum-annee>
+				<mois-valeur-max><?=($moisValeurMax!=null) ? $moisValeurMax->mois:"";?></mois-valeur-max>
+				<mois-valeur-min><?=($moisValeurMin!=null) ? $moisValeurMin->mois:"";?></mois-valeur-min>
+				<mois-max-tests><?=($moisMaxTests!=null) ? $moisMaxTests->mois:"";?></mois-max-tests>
+				<mois-min-tests><?=($moisMinTests!=null) ? $moisMinTests->mois:"";?></mois-min-tests>
+			</synthese>
+		</statistiques>
+	</statistiques-annee>
+
+<?php } else {
+	echo("Les arguments ne sont pas valides");
+} ?>
