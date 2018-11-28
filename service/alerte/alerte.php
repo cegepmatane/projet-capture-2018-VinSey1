b6066a08f24cd241f6c884771cd736f06e13c5f3
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-11-28 14:52:29 -0500

    Squelette du service pour l'alerte

---
+++ service/alerte/alerte.php
@@ -0,0 +1,19 @@
+<?php 
+	header("Content-type: text/xml");
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+?>
+
+
+<surveillance>
+	<actif>
+	
+	</actif>
+	<date-dernier-test>
+	
+	</date-dernier-test>
+	
+
+</surveillance>
+    
+
