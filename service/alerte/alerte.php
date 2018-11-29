52a0088cd403b5fcd53ae50727b20167e043cdca
Vincent Seyller <vseyller@laposte.net>
2018-11-29 01:14:34 -0500

    Test Alerte fonctionnel

--- service/alerte/alerte.php
+++ service/alerte/alerte.php
@@ -1,6 +1,5 @@
 <?php 
 	include "../accesseur/statistiqueDAO.php";
-	
 
 	$statDao = new StatistiqueDAO;
 
@@ -10,23 +9,10 @@
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
 	
 	$heureCourante = date('H');
-	
-
 ?>
 
-
 <surveillance>
-	<actif>
-	<?=($heureDernierTest<$heureCourante-1) ? 0:1;?>
-	</actif>
-	<heure-courante>
-	<?=$heureCourante ?>
-	</heure-courante>
-	<heure-dernier-test>
-	<?=$heureDernierTest->heure?>
-	</heure-dernier-test>
-	
-
+	<actif><?=($heureDernierTest->heure == $heureCourante) ? 1:0;?></actif>
+	<heure-courante><?=$heureCourante ?></heure-courante>
+	<heure-dernier-test><?=$heureDernierTest->heure?></heure-dernier-test>
 </surveillance>
-    
-
