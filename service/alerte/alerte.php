6c04339f879a5f607a20ea9f0ab93a9d68ece761
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-12-03 17:29:05 -0500

    Gestion de valeur dangereuse dans alerte

--- service/alerte/alerte.php
+++ service/alerte/alerte.php
@@ -4,9 +4,24 @@
 	$statDao = new StatistiqueDAO;
 
 	$heureDernierTest = $statDao->verifierDernierTest();
+	
+	$listeCapteursHeure = $statDao->recevoirListeCapteurHeure(date('m'), date('Y'), date('d'));
+	
+	$valeurDangereuse = 0;
+	foreach($listeCapteursHeure as $heure){
+		if($heure->maximumheure > 500 || $heure->minimumheure > 500 || $heure->moyenneheure > 500){
+			$valeurDangereuse = 1;
+		}
+
+		
+		
+		
+	}
 
 	header("Content-type: text/xml");
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
+	
+	
 	
 	$heureCourante = date('H');
 ?>
@@ -15,4 +30,5 @@
 	<actif><?=($heureDernierTest->heure == $heureCourante) ? 1:0;?></actif>
 	<heure-courante><?=$heureCourante ?></heure-courante>
 	<heure-dernier-test><?=$heureDernierTest->heure?></heure-dernier-test>
+	<valeur-dangereuse><?=$valeurDangereuse?></valeur-dangereuse>
 </surveillance>
